edaacb95efb137bcc59b61fd8f8161c3b372ecf5
c-robinson23 <147357577+c-robinson23@users.noreply.github.com>
2024-03-15 22:01:53 -0400

    Added axios integration so it shows the city weather

---
+++ src/Weather.js
@@ -0,0 +1,27 @@
+import React from "react";
+import axios from "axios";
+import { BallTriangle } from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${response.data.main.temp}˚F`
+    );
+  }
+  let apiKey = "85bbd3d16a2dfe0ecf253c7ae1e8fe03";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=imperial`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return (
+    <BallTriangle
+      height={100}
+      width={100}
+      radius={5}
+      color="#4fa94d"
+      ariaLabel="ball-triangle-loading"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+    />
+  );
+}
